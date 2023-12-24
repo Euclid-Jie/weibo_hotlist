a6a6dcd2278a360bc654aa454a1a94c6d1c7f22c
WeiJie Ou <ouweijie123@outlook.com>
2023-12-24 23:38:01 +0800

    modify setting

--- EuclidDataTools.py
+++ EuclidDataTools.py
@@ -2,8 +2,8 @@
 # @Time    : 2023/3/11 10:45
 # @Author  : Euclid-Jie
 # @File    : EuclidDataTools.py
-import os
 import pandas as pd
+from pathlib import Path
 
 
 class EuclidCsvTools:
@@ -11,12 +11,12 @@ class EuclidCsvTools:
     this class include tools used to precess csv file
     """
 
-    def __init__(self, subFolder: str = None, FileName: str = 'DemoOut.csv'):
+    def __init__(self, subFolder: str = None, FileName: str = "DemoOut.csv"):
         # para init
         self.subFolder = subFolder
         self.FileName = FileName
-        self.FullFilePath = None
-        self.FullFolderPath = None
+        self.FullFilePath: Path = None
+        self.FullFolderPath: Path = None
 
     def path_clear(self):
         """
@@ -24,14 +24,12 @@ class EuclidCsvTools:
         :return:
         """
         if self.subFolder:
-            if '\\' in self.subFolder:
-                self.FullFolderPath = os.getcwd() + self.subFolder
-            else:
-                self.FullFolderPath = os.getcwd() + '\\' + self.subFolder
+            self.FullFolderPath = Path("./", self.subFolder)
+            self.FullFilePath = Path(self.subFolder, self.FileName)
         else:
-            self.FullFolderPath = os.getcwd()
-        self.FullFilePath = os.path.join(self.FullFolderPath, self.FileName)
-        print('文件将存储在: {}'.format(self.FullFilePath))
+            self.FullFolderPath = Path("./")
+            self.FullFilePath = Path("./", self.FileName)
+        print("文件将存储在: {}".format(self.FullFilePath))
 
     def saveCsvFile(self, df, append=False):
         """
@@ -43,39 +41,35 @@ class EuclidCsvTools:
         if not self.FullFilePath:
             self.path_clear()
 
-        if os.path.exists(self.FullFolderPath):
-            # folder path exit
-            if append:
-                self.writeDf2Csv(df, self.FullFilePath)
-            else:
-                df.to_csv(self.FullFilePath, encoding='utf_8_sig', index=False)
+        self.FullFolderPath.mkdir(parents=True, exist_ok=True)
+        if append:
+            self.writeDf2Csv(df, self.FullFilePath)
         else:
-            # no dir exists then make one and save data to csv
-            os.mkdir(self.FullFolderPath)
-            if append:
-                self.writeDf2Csv(df, self.FullFilePath)
-            else:
-                df.to_csv(self.FullFilePath, encoding='utf_8_sig', index=False)
+            df.to_csv(self.FullFilePath, encoding="utf_8_sig", index=False)
 
     @classmethod
     def writeDf2Csv(cls, df, FullFilePath):
-        if not os.path.exists(FullFilePath):
-            # write out a new file with header
-            df.to_csv(FullFilePath, mode='w', encoding='utf_8_sig', header=True, index=False)
-        else:
+        if Path(FullFilePath).exists():
             # write after a exist file without header
-            df.to_csv(FullFilePath, mode='a', encoding='utf_8_sig', header=False, index=False)
+            df.to_csv(
+                FullFilePath, mode="a", encoding="utf_8_sig", header=False, index=False
+            )
+        else:
+            # write out a new file with header
+            df.to_csv(
+                FullFilePath, mode="w", encoding="utf_8_sig", header=True, index=False
+            )
 
 
 class CsvClient(EuclidCsvTools):
-    def __init__(self, subFolder: str = None, FileName: str = 'DemoOut.csv'):
+    def __init__(self, subFolder: str = None, FileName: str = "DemoOut.csv"):
         """
         :param subFolder:
         :param FileName:
         """
         super().__init__(subFolder=subFolder, FileName=FileName)
-        if FileName[-4:] != '.csv':
-            self.FileName = self.FileName + '.csv'
+        if FileName[-4:] != ".csv":
+            self.FileName = self.FileName + ".csv"
         self.path_clear()
 
     def insert_one(self, data):
--- start.sh
+++ start.sh
@@ -1,14 +1,1 @@
 python3 ./weibo_hot_list_serach.py
-
-year=`date +%Y `
-month=`date +%m `
-day=`date +%d `
-hour=`date +%H`
-now=$year-$month-$day-$hour
-
-
-git config --global user.email "ouweijie123@outlook.com"
-git config --global user.name "actioner"
-
-git add .
-git commit -m "$now"
